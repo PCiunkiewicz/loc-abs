7cf2b146d25ce58cfde7904a0718fe1c177bc29d
eshiAkalumhe14 <eshilama.akalumhe@ucalgary.ca>
2025-11-04 14:45:05 -0700

    Refactor logging setup and add system type definitions

--- frontend/dash_app.py
+++ frontend/dash_app.py
@@ -1,7 +1,12 @@
+"""Frontend Dash Application."""
 from dash import Dash, html
 import dash
 import dash_bootstrap_components as dbc
 import components_.header as header
+from utilities.logging import configure_logger
+from loguru import logger
+
+configure_logger(level="DEBUG")
 
 app = Dash(__name__, use_pages= True, external_stylesheets=[dbc.themes.LUX])
 
@@ -14,7 +19,7 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
-    print("Registered pages/routes:")
+    logger.debug("Registered pages/routes:")
     for p in dash.page_registry.values():
-        print(f"- {p['name']} -> {p['path']}")
+        logger.debug(f"- {p['name']} -> {p['path']}")
     app.run(debug=True)
